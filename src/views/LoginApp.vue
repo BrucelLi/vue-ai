61641a9eb61d6ff6d602ef1e4f24b7bc510a4ad1
li.xiaolong <li.xiaolong@shuluntc.com>
2025-09-26 17:48:43 +0800

    add login context

--- src/views/LoginApp.vue
+++ src/views/LoginApp.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const loginState = ref<boolean>(false)
+const LoginCallbakc = () => {
+  loginState.value = true
+}
+const goBack = () => {
+  loginState.value = false
+}
+</script>
 <template>
-  <div>login</div>
+  <div>
+    <h3>login</h3>
+    <div><button @click="LoginCallbakc">login</button></div>
+    <div><button @click="goBack">cancel</button></div>
+  </div>
 </template>
